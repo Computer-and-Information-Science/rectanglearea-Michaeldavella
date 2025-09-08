f0c9567f63981c97784000d85f3d2bd41f712ef3
Michael D'Avella <michaeldavella2006@gmail.com>
2025-09-08 01:53:08 +0000

    Wrote code for rectangle area/properties.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main() {
+  //Rectangle Dimensions
   const double length = 7.2;
   const double width = 1.3;
+
+  //Calculations
+  double area = length * width;
+  double perimeter = 2 * (length + width);
+
+  //Output
+  cout << "Rectangle Properties: " << endl;
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
+
+
+  
 
 
 }
